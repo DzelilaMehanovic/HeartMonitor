c718ad3ee2e0b2edfdc8bcbc65b93221cb590ad4
DzelilaMehanovic <dzelila.mehanovic@stu.ibu.edu.ba>
2017-12-03 20:59:15 +0100

    .

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-var express = require('express'); 
-var app = express(); 
+var express = require('express');
+var app = express();
+
+var mongojs = require('mongojs');
+var db = mongojs('localhost:27017/heart_monitor', ['diseases'])
+
+/*var body_parser = require('body-parser');
+app.use(body_parser.json());*/
 
 app.use(express.static(__dirname+'/public'));
 
-app.get('/HeartMonitor', function(req, res){ 
-  res.send('Hello;'); 
-}); 
-app.listen(3000); 
-console.log('running on 3000'); 
+app.get('/HeartMonitor', function(req, res){
+  db.diseases.find(function(err, docs){
+    console.log(docs);
+    res.json(docs);
+  })
+});
+
+
+
+
+app.listen(3000);
+console.log('running on 3000');
